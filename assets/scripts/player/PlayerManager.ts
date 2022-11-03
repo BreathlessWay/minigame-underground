b73061fb0dc0acbc419908f76d65177b7275ea89
BreathlessWay <731005087@qq.com>
2022-11-03 14:50:18 +0800

    style: lesson 12

--- assets/scripts/player/PlayerManager.ts
+++ assets/scripts/player/PlayerManager.ts
@@ -1,24 +1,85 @@
 import {
 	_decorator,
+	Animation,
+	animation,
+	AnimationClip,
 	Component,
 	Sprite,
-	UITransform,
-	Animation,
-	AnimationClip,
-	animation,
 	SpriteFrame,
+	UITransform,
 } from "cc";
 
 import ResourceManager from "db://assets/stores/ResourceManager";
 
+import EventManager from "db://assets/stores/EventManager";
+
 import { TILE_HEIGHT, TILE_WIDTH } from "db://assets/scripts/tile/TileManager";
+import { CONTROLLER_ENUM, EVENT_ENUM } from "db://assets/enums";
 
 const { ccclass, property } = _decorator,
 	ANIMATION_SPEED = 1 / 8;
 
 @ccclass("PlayerManager")
 export class PlayerManager extends Component {
+	x = 0;
+	y = 0;
+	targetX = 0;
+	targetY = 0;
+	private readonly speed = 1 / 10;
+
+	update() {
+		this.updatePosition();
+		this.node.setPosition(
+			this.x * TILE_WIDTH - TILE_WIDTH * 1.5,
+			-this.y * TILE_HEIGHT + TILE_HEIGHT * 1.5
+		);
+	}
+
 	async init() {
+		await this.render();
+		EventManager.Instance.on(EVENT_ENUM.PLAYER_CTRL, this.move, this);
+	}
+
+	updatePosition() {
+		if (this.x < this.targetX) {
+			this.x += this.speed;
+		}
+		if (this.x > this.targetX) {
+			this.x -= this.speed;
+		}
+
+		if (this.y < this.targetY) {
+			this.y += this.speed;
+		}
+		if (this.y > this.targetY) {
+			this.y -= this.speed;
+		}
+
+		if (
+			Math.abs(this.targetX - this.x) <= 0.1 &&
+			Math.abs(this.targetY - this.y) <= 0.1
+		) {
+			this.x = this.targetX;
+			this.y = this.targetY;
+		}
+	}
+
+	move(direction: CONTROLLER_ENUM) {
+		if (direction === CONTROLLER_ENUM.TOP) {
+			this.targetY -= 1;
+		}
+		if (direction === CONTROLLER_ENUM.BOTTOM) {
+			this.targetY += 1;
+		}
+		if (direction === CONTROLLER_ENUM.LEFT) {
+			this.targetX -= 1;
+		}
+		if (direction === CONTROLLER_ENUM.RIGHT) {
+			this.targetX += 1;
+		}
+	}
+
+	async render() {
 		const sprite = this.addComponent(Sprite);
 		sprite.sizeMode = Sprite.SizeMode.CUSTOM;
 
--- assets/scripts/ui/ControlManager.ts
+++ assets/scripts/ui/ControlManager.ts
@@ -1,14 +1,14 @@
-import { _decorator, Component, Node } from "cc";
+import { _decorator, Component, Event } from "cc";
 
 import EventManager from "db://assets/stores/EventManager";
 
 import { EVENT_ENUM } from "db://assets/enums";
 
-const { ccclass, property } = _decorator;
+const { ccclass } = _decorator;
 
 @ccclass("ControlManager")
 export class ControlManager extends Component {
-	handleControl() {
-		EventManager.Instance.emit(EVENT_ENUM.NEXT_LEVEL);
+	handleControl(event: Event, type: string) {
+		EventManager.Instance.emit(EVENT_ENUM.PLAYER_CTRL, type);
 	}
 }
